bc2827d0f73fbdd9a5d5eaf6c8430a3ed8145999
devy0106 <sehga060@umn.edu>
2026-03-26 13:24:38 -0500

    Update checkpoint10.py

--- checkpoint10.py
+++ checkpoint10.py
@@ -5,11 +5,12 @@ from xarm.wrapper import XArmAPI
 
 from utils.vis_utils import draw_pose_axes
 from utils.zed_camera import ZedCamera
+from checkpoint0 import get_transform_camera_robot
 from checkpoint1 import grasp_cube, place_cube, GRIPPER_LENGTH
 from checkpoint4 import STACK_HEIGHT
 
 stacking_order = ['red cube', 'green cube', 'blue cube']   # From top to bottom
-robot_ip = ''
+robot_ip = '192.168.1.182'
 
 def main():
 
@@ -36,6 +37,59 @@ def main():
         point_cloud = zed.point_cloud
 
         # TODO
+        # compute cam to robot transf
+        t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+        if t_cam_robot is None:
+            print("Failed to cpmpute cam robot transform")
+            return
+
+        cube_pose_detector.camera_pose = t_cam_robot
+
+        # detect all cubes
+        poses = {}
+        for cube in stacking_order:
+            result = cube_pose_detector.get_transforms([cv_image, point_cloud], cube)
+            if result is None:
+                print(f"Could not detect {cube}")
+                return
+            t_robot, t_cam = result
+            poses[cube] = (t_robot, t_cam)
+
+        # Visualization
+        for cube in stacking_order:
+            _, t_cam = poses[cube]
+            draw_pose_axes(cv_image, camera_intrinsic, t_cam)
+
+        cv2.imshow('Checkpoint 10', cv_image)
+        print("Press 'k' to execute stacking")
+        if cv2.waitKey(0) != ord('k'):
+            return
+        cv2.destroyAllWindows()
+
+        # stacking logic (bottom -> top)
+        base_cube = stacking_order[-1]
+        t_robot_base, _ = poses[base_cube]
+
+        for i in reversed(range(len(stacking_order) - 1)):
+            cube = stacking_order[i]
+            t_robot_cube, _ = poses[cube]
+
+            # pick cube
+            grasp_cube(arm, t_robot_cube)
+            time.sleep(0.5)
+
+            # compute stacking pose
+            t_robot_stack = t_robot_base.copy()
+            t_robot_stack[2, 3] -= STACK_HEIGHT
+            t_robot_stack[:3, :3] = t_robot_base[:3, :3]
+
+            # place cube
+            place_cube(arm, t_robot_stack)
+
+            # update base for next level
+            t_robot_base = t_robot_stack.copy()
+            time.sleep(0.5)
+
     
     finally:
         # Close Lite6 Robot
