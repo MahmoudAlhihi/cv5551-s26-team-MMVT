699da919a76c96cab6dde86deb6d5453bbb53d48
Mahmoud Alhihi <mahmoudalhihi7@gmail.com>
2026-03-26 10:51:04 -0500

    Done with checkpoint 5

--- checkpoint5.py
+++ checkpoint5.py
@@ -2,14 +2,17 @@ from checkpoint3 import CubePoseDetector
 
 import cv2, time
 from xarm.wrapper import XArmAPI
+import numpy
 
 from utils.vis_utils import draw_pose_axes
 from utils.zed_camera import ZedCamera
+from checkpoint0 import get_transform_camera_robot
 from checkpoint1 import grasp_cube, place_cube, GRIPPER_LENGTH
 from checkpoint4 import STACK_HEIGHT
 
+
 stacking_order = ['red cube', 'green cube', 'blue cube']   # From top to bottom
-robot_ip = ''
+robot_ip = '192.168.1.182'
 
 def main():
 
@@ -34,7 +37,39 @@ def main():
         # Get Observation
         cv_image = zed.image
 
-        # TODO
+        t_cam_robot = get_transform_camera_robot(cv_image, camera_intrinsic)
+        if t_cam_robot is None:
+            return
+        cube_pose_detector.camera_pose = t_cam_robot
+
+        poses = {}
+        for cube in stacking_order:
+            result = cube_pose_detector.get_transforms(cv_image, cube)
+            if result is None:
+                return
+            t_robot, t_cam = result
+            poses[cube] = (t_robot, t_cam)
+        
+        for cube in stacking_order:
+            _, t_cam = poses[cube]
+            draw_pose_axes(cv_image, camera_intrinsic, t_cam)
+
+        cv2.namedWindow("Verifying cube poses", cv2.WINDOW_NORMAL)
+        cv2.imshow("Verifying cube poses", cv_image)
+        key = cv2.waitKey(0)
+
+        if key == ord ('k'):
+            cv2.destroyAllWindows()
+            base_cube = stacking_order[-1]
+            t_robot_base, _ = poses[base_cube]
+
+            for i, cube in enumerate(reversed(stacking_order[:-1]), start=1):
+                t_robot_cube, _ = poses[cube]
+                grasp_cube(arm, t_robot_cube)
+
+                stack_target = numpy.copy(t_robot_base)
+                stack_target[2, 3] -= STACK_HEIGHT * i
+                place_cube(arm, stack_target)
     
     finally:
         # Close Lite6 Robot
