8b3e9008115c37bcaf6b06e2eab9c7f19929eb1a
rob <mattbr0n3@gmail.com>
2026-03-31 13:19:31 -0500

    Updated code

--- save_test_image.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from utils.zed_camera import ZedCamera
-
-zed = ZedCamera()
-img = zed.image
-np.save('/tmp/test_image.npy', img)
-print(f"Saved image shape: {img.shape}")
-zed.close()
